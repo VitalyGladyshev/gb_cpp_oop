41a21f9c8f53fcfa3b94ed6c64c766ffa38c6ab0
VitalyGladyshev <viv232@yandex.ru>
2022-02-19 17:03:50 +0300

    ДЗ 4 начал

---
+++ hw4/main.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Гладышев ВВ on 19.02.2022.
+// ООП на примере C++
+// ДЗ к уроку 4 Отношения между объектами в С++
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//Задание 1
+
+//Задание 2
+
+//Задание 3
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+// Задание 1
+    cout << "Задание 1" << endl;
+
+    cout << endl;
+
+// Задание 2
+    cout << "Задание 2" << endl;
+
+    cout << endl;
+
+// Задание 3
+    cout << "Задание 3" << endl;
+
+
+}
