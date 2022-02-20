ac9305234343a5ab86e047a83d745ad097ae3ea1
VitalyGladyshev <viv232@yandex.ru>
2022-02-20 23:55:12 +0300

    ДЗ 5 готово

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -6,25 +6,58 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-//Класс
+//Шаблонный класс пара для элементов одинакового типа
+template<typename T>
 class Pair1
 {
+    T _first;
+    T _second;
 
+public:
+    Pair1(T first, T second) : _first(first), _second(second) {}
+
+    T first() const
+    {
+        return _first;
+    }
+
+    T second() const
+    {
+        return _second;
+    }
 };
 
-//Класс
+//Шаблонный класс пара для двух разных типов
+template<typename T1, typename T2>
 class Pair
 {
+    T1 _first;
+    T2 _second;
 
+public:
+    Pair(T1 first, T2 second) : _first(first), _second(second) {}
+
+    T1 first() const
+    {
+        return _first;
+    }
+
+    T2 second() const
+    {
+        return _second;
+    }
 };
 
 //Класс
-class StringValuePair
+template<typename T>
+class StringValuePair : public Pair<string, T>
 {
-
+public:
+    StringValuePair(string first, T second) : Pair<string, T>(first, second) {}
 };
 
 int main()
@@ -34,32 +67,28 @@ int main()
 // Задание 1
     cout << "Задание 1" << endl;
 
-//    Pair1<int> p1(6, 9);
-//    cout << "\tPair: " << p1.first() << ' ' << p1.second() << endl;
-//
-//    const Pair1<double> p2(3.4, 7.8);
-//    cout << "\tPair: " << p2.first() << ' ' << p2.second() << endl;
+    Pair1<int> p1(6, 9);
+    cout << "\tPair: " << p1.first() << ' ' << p1.second() << endl;
 
+    const Pair1<double> p2(3.4, 7.8);
+    cout << "\tPair: " << p2.first() << ' ' << p2.second() << endl;
     cout << endl;
 
 // Задание 2
     cout << "Задание 2" << endl;
 
-//    Pair<int, double> p1(6, 7.8);
-//    cout << "\tPair: " << p1.first() << ' ' << p1.second() << endl;
-//
-//    const Pair<double, int> p2(3.4, 5);
-//    cout << "\tPair: " << p2.first() << ' ' << p2.second() << endl;
+    Pair<int, double> p1_2(6, 7.8);
+    cout << "\tPair: " << p1_2.first() << ' ' << p1_2.second() << endl;
 
+    const Pair<double, int> p2_2(3.4, 5);
+    cout << "\tPair: " << p2_2.first() << ' ' << p2_2.second() << endl;
     cout << endl;
 
 // Задание 3
     cout << "Задание 3" << endl;
 
-//    StringValuePair<int> svp("Amazing", 7);
-//    cout << "\tPair: " << svp.first() << ' ' << svp.second() << endl;
-
-    cout << endl;
+    StringValuePair<int> svp("Amazing", 7);
+    cout << "\tPair: " << svp.first() << ' ' << svp.second() << endl;
 
     return 0;
 }
