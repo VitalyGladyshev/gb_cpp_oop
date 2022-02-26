ade7a078a6ae3df49dec3ea53df880333a33cb79
VitalyGladyshev <viv232@yandex.ru>
2022-02-26 21:52:04 +0300

    ДЗ 6 начал

---
+++ hw6/main.cpp
@@ -0,0 +1,41 @@
+//
+// Created by Гладышев ВВ on 26.02.2022.
+// ООП на примере C++
+// ДЗ к уроку 6 Совместное использование функций и методов
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+// Задание 1
+    cout << "Задание 1" << endl;
+
+    cout << endl;
+
+// Задание 2
+    cout << "Задание 2" << endl;
+
+    cout << endl;
+
+// Задание 3
+    cout << "Задание 3" << endl;
+
+    cout << endl;
+
+// Задание 4
+    cout << "Задание 4" << endl;
+
+    cout << endl;
+
+// Задание 5
+    cout << "Задание 5" << endl;
+
+    cout << endl;
+}
