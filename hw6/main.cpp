9e0e1407a8ae0770ee3a99610af22a678f764599
VitalyGladyshev <viv232@yandex.ru>
2022-02-27 01:10:32 +0300

    ДЗ 6 начал

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -5,10 +5,20 @@
 //
 
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
+//Манипулятор вывода двойного перевода строки
+template< class CharT, class Traits >
+std::basic_ostream<CharT, Traits>& endll( std::basic_ostream<CharT, Traits>& os )
+{
+    os.put(os.widen('\n'));
+    os.put(os.widen('\n'));
+    os.flush();
 
+    return os;
+}
 
 int main()
 {
@@ -17,12 +27,31 @@ int main()
 // Задание 1
     cout << "Задание 1" << endl;
 
-    cout << endl;
+    bool getException;
+    string strValue;
+    int number;
+
+    do
+    {
+        cout << "\tВведите целое число: ";
+        cin >> number;
+        if (cin.good())
+            getException = false;
+        else
+        {
+            getException = true;
+            cout << "\tДанное значение не является целым числом!" << endl;
+        }
+        cin.clear();
+        _flushall();
+    }
+    while(getException);
+    cout << "\tВведено целое число: " << number << endl << endl;
 
 // Задание 2
     cout << "Задание 2" << endl;
-
-    cout << endl;
+    cout << "\tМанипулятор endll - двойной перевод строки" << endll;
+    cout << "\tНовая строка!" << endll;
 
 // Задание 3
     cout << "Задание 3" << endl;
