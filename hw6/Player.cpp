9e3f4a54c4e9f36d8c7efe227b3db3f89973c37e
VitalyGladyshev <viv232@yandex.ru>
2022-02-27 12:37:41 +0300

    ДЗ 6 задания 1-3, (4-5 в процессе)

---
+++ hw6/Player.cpp
@@ -0,0 +1,83 @@
+//
+// Created by Гладышев ВВ on 20.02.2022.
+//
+
+#include <iostream>
+
+#include "Player.h"
+
+//Указывает, что у игрока перебор
+bool GenericPlayer::IsBoosted()
+{
+    if (GetTotal() > 11)
+        _boosted = true;
+    else
+        _boosted = false;
+    return _boosted;
+}
+
+//Объявляет, что у игрока перебор
+void GenericPlayer::Bust()
+{
+    cout << "\tУ игрока " << _name << " перебор!" << endl;
+}
+
+//Возвращает имя игрока
+string GenericPlayer::GetName() const
+{
+    return _name;
+}
+
+//Конструктор
+Player::Player(string name, initializer_list<Card*> cards) : GenericPlayer(name, cards) {}
+
+//Спрашивает у пользователя, нужна ли ему еще одна карта и возвращает ответ пользователя
+bool Player::IsHitting() const
+{
+    cout << "\tВзять карту (д/н)?" << endl;
+    string userAnswer;
+    cin >> userAnswer;
+    vector<string> yesAnswer {"Д", "д", "Да", "да", "y", "Y"};
+    bool yesAnswerFlag = false;
+
+    for (auto answer : yesAnswer)
+        if (userAnswer == answer)
+            yesAnswerFlag = true;
+
+    return yesAnswerFlag;
+}
+
+//Выводит на экран имя игрока и сообщение, что он выиграл
+void Player::Win() const
+{
+    cout << "\tИгрок " << GetName() << " выиграл!" << endl;
+}
+
+//Выводит на экран имя игрока и сообщение, что он проиграл
+void Player::Lose() const
+{
+    cout << "\tУ игрока " << GetName() << " перебор!" << endl;
+}
+
+//Выводит на экран имя игрока и сообщение, что он сыграл вничью
+void Player::Push() const
+{
+    cout << "\tУ игрока " << GetName() << " перебор!" << endl;
+}
+
+//Конструктор
+House::House(string name, initializer_list<Card*> cards) : GenericPlayer(name, cards)
+{
+
+}
+
+//Спрашивает у пользователя, нужна ли ему еще одна карта и возвращает ответ пользователя
+bool House::IsHitting() const
+{
+
+}
+//Метод переворачивает первую карту дилера
+void House::FlipFirstCard()
+{
+
+}
---
+++ hw6/Player.h
@@ -0,0 +1,62 @@
+//
+// Created by Гладышев ВВ on 20.02.2022.
+//
+
+#ifndef GB_CPP_OOP_PLAYER_H
+#define GB_CPP_OOP_PLAYER_H
+
+#include "..\hw4\Card.h"
+#include "..\hw4\Hand.h"
+
+//BlackJack Обобщенный класс - игрок
+class GenericPlayer : public Hand
+{
+private:
+    string _name;   //Имя игрока
+    bool _boosted;  //Флаг перебор
+
+public:
+    //Конструктор
+    GenericPlayer(string name, initializer_list<Card*> cards): Hand(cards), _name(name), _boosted(false) {}
+
+    //Указывает, нужна ли игроку еще одна карта. Чистая виртуальная функция
+    virtual bool IsHitting() const = 0;
+    //Указывает, что у игрока перебор
+    bool IsBoosted();
+    //Объявляет, что у игрока перебор
+    void Bust();
+    //Возвращает имя игрока
+    string GetName() const;
+};
+
+//Класс Игрок
+class Player : public GenericPlayer
+{
+public:
+    //Конструктор
+    Player(string name, initializer_list<Card*> cards);
+
+    //Спрашивает у пользователя, нужна ли ему еще одна карта и возвращает ответ пользователя
+    virtual bool IsHitting() const;
+    //Выводит на экран имя игрока и сообщение, что он выиграл
+    void Win() const;
+    //Выводит на экран имя игрока и сообщение, что он проиграл
+    void Lose() const;
+    //Выводит на экран имя игрока и сообщение, что он сыграл вничью
+    void Push() const;
+};
+
+//Класс Игрок
+class House : public GenericPlayer
+{
+public:
+    //Конструктор
+    House(string name, initializer_list<Card*> cards);
+
+    //Спрашивает у пользователя, нужна ли ему еще одна карта и возвращает ответ пользователя
+    virtual bool IsHitting() const;
+    //Метод переворачивает первую карту дилера
+    void FlipFirstCard();
+}
+
+#endif //GB_CPP_OOP_PLAYER_H
