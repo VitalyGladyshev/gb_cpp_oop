6a09d022519dcb8d93f5d8ac4a2f963399b17813
Vitaly Gladyshev <viv232@yandex.ru>
2022-03-05 16:44:32 +0300

    ДЗ 8 начал

---
+++ hw8/main.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Гладышев ВВ on 05.03.2022.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+// Задание 1
+    cout << "Задание 1" << endl;
+
+    cout << endl;
+// Задание 2
+    cout << "Задание 2" << endl;
+
+    cout << endl;
+// Задание 3
+    cout << "Задание 3" << endl;
+
+    cout << endl;
+    return 0;
+}
